e76b47d937ba2c7907c1d819264fd2ebb0d86250
Gilbert <gilbertduenas@users.noreply.github.com>
2020-06-15 00:15:21 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,21 @@
+def snail(snail_map):
+    final = []
 
+    while len(snail_map) > 0:
+        final += snail_map[0]
+        del snail_map[0]
+
+        if len(snail_map) > 0:
+            for i in snail_map:
+                final += [i[-1]]
+                i.pop()
+
+            if snail_map[-1]:
+                final += snail_map[-1][::-1]
+                snail_map.pop()
+
+            for i in reversed(snail_map):
+                final += [i[0]]
+                del i[0]
+
+    return final
